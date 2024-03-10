5a276757df476ace3e643a37dec4682b245cb581
Sajid Mushtaq <138590004+sajidmustaq@users.noreply.github.com>
2024-03-10 09:11:10 +0000

    Todo update

--- Java Script/To Do List/script.js
+++ Java Script/To Do List/script.js
@@ -1,16 +1,24 @@
-
 var inputElement = document.getElementById("input");
 var addButton = document.getElementById("add");
 var paraDiv = document.querySelector(".para");
 
+// Load tasks from local storage on page load
+window.onload = function () {
+  var savedTasks = localStorage.getItem("tasks");
+  if (savedTasks) {
+    paraDiv.innerHTML = savedTasks;
+    setupDeleteButtons(); // Reattach delete button event listeners
+  }
+};
+
 addButton.addEventListener("click", () => {
   var inputValue = inputElement.value;
   if (inputValue === "") {
-    alert("Please Enter Task"); // Fixed typo in the alert message
+    alert("Please Enter Task");
   } else {
     var newPara = document.createElement("p");
     newPara.textContent = inputValue;
-    
+
     // Delete button
     var delBtn = document.createElement("button");
     delBtn.innerText = "Delete";
@@ -18,10 +26,31 @@ addButton.addEventListener("click", () => {
 
     delBtn.addEventListener("click", () => {
       paraDiv.removeChild(newPara);
+      updateLocalStorage();
     });
 
     paraDiv.appendChild(newPara);
     newPara.appendChild(delBtn);
     inputElement.value = "";
+
+    // Save tasks to local storage
+    updateLocalStorage();
+    setupDeleteButtons(); // Reattach delete button event listeners
   }
 });
+
+function updateLocalStorage() {
+  var tasks = paraDiv.innerHTML;
+  localStorage.setItem("tasks", tasks);
+}
+
+function setupDeleteButtons() {
+  var deleteButtons = document.querySelectorAll(".delete-button");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      var parentPara = button.parentElement;
+      paraDiv.removeChild(parentPara);
+      updateLocalStorage();
+    });
+  });
+}
