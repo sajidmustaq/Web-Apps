55105f96e4e970122cc7e78cce947edb91762548
Sajid Mushtaq <138590004+sajidmustaq@users.noreply.github.com>
2024-03-15 19:02:40 +0000

    fb post

--- Java Script/Facbook Post/app.js
+++ Java Script/Facbook Post/app.js
@@ -1,16 +1,26 @@
 document.querySelectorAll('.dropdown-toggle').forEach(item => {
-    item.addEventListener('click', event => {
-   
-      if(event.target.classList.contains('dropdown-toggle') ){
-        event.target.classList.toggle('toggle-change');
+  item.addEventListener('click', event => {
+      if (event.target.classList.contains('dropdown-toggle')) {
+          event.target.classList.toggle('toggle-change');
+      } else if (event.target.parentElement.classList.contains('dropdown-toggle')) {
+          event.target.parentElement.classList.toggle('toggle-change');
       }
-      else if(event.target.parentElement.classList.contains('dropdown-toggle')){
-        event.target.parentElement.classList.toggle('toggle-change');
-      }
-    })
-  });
-
-  let creatClass = document.querySelector("#craetClass")
-  creatClass.addEventListener("click",()=>{
-    console.log("Sajid");
   })
+});
+
+let createPostButton = document.querySelector("#createPost");
+createPostButton.addEventListener("click", () => {
+  let postContainer = document.getElementById("cards_landscape_wrap-2");
+
+  let newBox = document.createElement("div");
+  newBox.classList.add("col-md-12", "mt-3");
+  newBox.innerHTML = `
+      <div class="card-flyer">
+          <div class="text-box">
+              <h1 class="card-title">Facebook Post</h1>
+              <a href="#" class="card-text" id="createPost">Create Post</a>
+          </div>
+      </div>`;
+
+  postContainer.appendChild(newBox);
+});
